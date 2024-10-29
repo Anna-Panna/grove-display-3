be30cceb953ad60241e02ac644a81cdae70cd47c
Anna-Panna <anna.m.neumeier@icloud.com>
2024-10-29 11:32:55 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+let _4digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
+_4digit.set(4)
 basic.forever(function () {
-	
+    for (let Index = 0; Index <= 100; Index++) {
+        _4digit.show(Index)
+        basic.pause(500)
+    }
 })
